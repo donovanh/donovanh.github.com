12f9b67f1e6d55584e128738f30904e8ca7e3a37
Donovan Hutchinson <donovanh@gmail.com>
2012-07-23 00:49:55 +0100

    Bug fix

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,10 @@
 (function(){
 	
 	// On load, show the bouncer
-	$('.bouncer-container').append($('#bouncer-template').html());
-	$('.bouncer-container').find('.bouncer').addClass('move-bouncer');
-	$('.bouncer-container').find('.mask').addClass('move-mask');
+	if (!$('html').hasClass('no-js')) {
+		$('.bouncer-container').append($('#bouncer-template').html());
+		$('.bouncer-container').find('.bouncer').addClass('move-bouncer');
+		$('.bouncer-container').find('.mask').addClass('move-mask');
+	}
 	
 }());
