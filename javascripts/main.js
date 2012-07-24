107a03f1095d878292b2565cc84d92d0f4adb99c
Donovan Hutchinson <donovanh@gmail.com>
2012-07-24 08:47:42 +0100

    Adding pygments and some general style improvements

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,9 +2,16 @@
 	
 	// On load, show the bouncer
 	if (!$('html').hasClass('no-js')) {
-		$('.bouncer-container').append($('#bouncer-template').html());
-		$('.bouncer-container').find('.bouncer').addClass('move-bouncer');
+		$('.bouncer-container a').append($('#bouncer-template').html());
+		var movingBouncer = $('.bouncer-container').find('.bouncer').addClass('move-bouncer');
+		setTimeout(function() {
+		    movingBouncer.removeClass('move-bouncer').addClass('bouncer-end-position');
+		}, 2000);
 		$('.bouncer-container').find('.mask').addClass('move-mask');
 	}
+	
+	$('.bouncer-container a').hover(function(e) {
+		$(e.target).toggleClass('hovered');
+	});
 	
 }());
