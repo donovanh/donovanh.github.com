1a83cd5fbdc93d03fd09c1673099a7bf0fa8a1d8
Donovan Hutchinson <donovanh@gmail.com>
2012-07-24 22:01:05 +0100

    Browser test for csstransitions added for the top animation

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,15 @@
 (function(){
 	
 	// On load, show the bouncer
-	if (!$('html').hasClass('no-js')) {
+	if ($('html').hasClass('csstransitions') && $('html').hasClass('borderradius')) {
 		$('.bouncer-container a').append($('#bouncer-template').html());
 		var movingBouncer = $('.bouncer-container').find('.bouncer').addClass('move-bouncer');
 		setTimeout(function() {
 		    movingBouncer.removeClass('move-bouncer').addClass('bouncer-end-position');
 		}, 2000);
 		$('.bouncer-container').find('.mask').addClass('move-mask');
+	} else {
+		$('.bouncer-container').addClass('old');
 	}
 	
 }());
