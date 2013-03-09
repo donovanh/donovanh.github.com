74868de8e1297e94f1105b76bda0c54c8fbdbfe0
Donovan Hutchinson <don@converser.io>
2013-03-09 17:21:54 +0000

    Some slides added

---
+++ talks/node-intro/examples/hello-world.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('OMG Bacon\n');
+});
+
+server.listen(1337, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:1337/');
